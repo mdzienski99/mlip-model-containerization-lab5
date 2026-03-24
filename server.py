fe3a25e05e936c216f75f09f3791b9ff813cbb77
mdzienski99 <mdzienski2018@fau.edu>
2026-03-24 13:49:03 +0100

    Completed Docker ML lab

--- server.py
+++ server.py
@@ -4,27 +4,27 @@ import joblib
 
 app = Flask(__name__)
 
-# TODO: Load the trained model from the shared volume (use the correct path)
-model = ...
+# Load the trained model from the shared volume
+model = joblib.load('/app/models/iris_model.pkl')
 
-# TODO: Add request method 
-@app.route('/predict', methods=[''])
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    # TODO: Get the input array from the request body
+    # Get the input array from the request body
     get_json = request.get_json()
     iris_input = get_json['input']
 
-    # TODO: Make prediction using the model 
-    # HINT: use np.array().reshape(1, -1) to convert input to 2D array
-    prediction = ...
+    # Make prediction using the model
+    prediction = model.predict(np.array(iris_input).reshape(1, -1))
 
-    # TODO: Return the prediction as a response
-    return ...
+    # Return the prediction as a response
+    return jsonify({"prediction": prediction[0]})
+
 
 @app.route('/')
 def hello():
     return 'Welcome to Docker Lab'
 
+
 if __name__ == '__main__':
-    # TODO: Run the Flask app (bind it to port 8080 or anyt other port)
     app.run(debug=True, port=8080, host='0.0.0.0')
